8e3bc0b91f5c88b380b28e847a8a471a351c6772
Jmactiag <jessie.macatiag@gmail.com>
2018-02-08 18:37:14 -0800

    add base logic code to liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,33 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
+var fs = require('fs');
+var request = require('request');
+var spotify = require('spotify');
+var twitter = require('twitter');
+var keys = require('./keys.js');
+
+// Get user input from terminal
+var liriArgs = process.argv.slice(2);
+var liriCommand = liriArgs[0];
+var liriData = liriArgs[1];
+
+
+// console.log(liriArgs);
+// console.log(liriCommand);
+// console.log(liriData);
+// console.log(process.argv);
+
+// liri function to determine what process needs to be run
+function liri(liriCommand){
+	if (liriCommand==="my-tweets"){
+		twitter();
+	}
+
+	else if (liriCommand==="spotify-this-song"){
+		spotify();
+	}
+
+	else if (liriCommand==="movie-this"){
+		movie();
+	}
+
+}
